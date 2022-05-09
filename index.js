8078ce605625e0b94c003eafbb10d6c8d1ef25e3
djpdim <dimospagkas@yahoo.gr>
2022-05-08 20:03:30 -0400

    update pseudo code and instructions to the content

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const fs = require("fs")
 const inquirer = require("inquirer")
 const teamArray = []
 
+//link express
 const express = require("express")
 const app = express()
 app.use(express.static("public"))
 
+//start manager prompt.
 const addManager = async () => {
     const managerInput = await inquirer.prompt([
         {
@@ -72,6 +74,7 @@ const addManager = async () => {
     console.log(manager)
 }
 
+//add employee
 const addEmployee = () => {
     console.log(`
     =================
@@ -79,111 +82,114 @@ const addEmployee = () => {
     =================
     `)
 
-    return inquirer
-        .prompt([
-            {
-                type: "list",
-                name: "role",
-                message: "Please choose your employee's role",
-                choices: ["Engineer", "Intern"],
-            },
-            {
-                type: "input",
-                name: "name",
-                message: "What's the name of the employee?",
-                validate: nameInput => {
-                    if (nameInput) {
-                        return true
-                    } else {
-                        console.log("Please enter an employee's name!")
-                        return false
-                    }
+    return (
+        inquirer
+            .prompt([
+                {
+                    type: "list",
+                    name: "role",
+                    message: "Please choose your employee's role",
+                    choices: ["Engineer", "Intern"],
                 },
-            },
-            {
-                type: "input",
-                name: "id",
-                message: "Please enter the employee's ID.",
-                validate: nameInput => {
-                    if (isNaN(nameInput)) {
-                        console.log("Please enter the employee's ID!")
-                        return false
-                    } else {
-                        return true
-                    }
+                {
+                    type: "input",
+                    name: "name",
+                    message: "What's the name of the employee?",
+                    validate: nameInput => {
+                        if (nameInput) {
+                            return true
+                        } else {
+                            console.log("Please enter an employee's name!")
+                            return false
+                        }
+                    },
                 },
-            },
-            {
-                type: "input",
-                name: "email",
-                message: "Please enter the employee's email.",
-                validate: email => {
-                    valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
-                    if (valid) {
-                        return true
-                    } else {
-                        console.log("Please enter an email!")
-                        return false
-                    }
+                {
+                    type: "input",
+                    name: "id",
+                    message: "Please enter the employee's ID.",
+                    validate: nameInput => {
+                        if (isNaN(nameInput)) {
+                            console.log("Please enter the employee's ID!")
+                            return false
+                        } else {
+                            return true
+                        }
+                    },
                 },
-            },
-            {
-                type: "input",
-                name: "github",
-                message: "Please enter the employee's github username.",
-                when: input => input.role === "Engineer",
-                validate: nameInput => {
-                    if (nameInput) {
-                        return true
-                    } else {
-                        console.log("Please enter the employee's github username!")
-                    }
+                {
+                    type: "input",
+                    name: "email",
+                    message: "Please enter the employee's email.",
+                    validate: email => {
+                        valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+                        if (valid) {
+                            return true
+                        } else {
+                            console.log("Please enter an email!")
+                            return false
+                        }
+                    },
                 },
-            },
-            {
-                type: "input",
-                name: "school",
-                message: "Please enter the intern's school",
-                when: input => input.role === "Intern",
-                validate: nameInput => {
-                    if (nameInput) {
-                        return true
-                    } else {
-                        console.log("Please enter the intern's school!")
-                    }
+                {
+                    type: "input",
+                    name: "github",
+                    message: "Please enter the employee's github username.",
+                    when: input => input.role === "Engineer",
+                    validate: nameInput => {
+                        if (nameInput) {
+                            return true
+                        } else {
+                            console.log("Please enter the employee's github username!")
+                        }
+                    },
                 },
-            },
-            {
-                type: "confirm",
-                name: "confirmAddEmployee",
-                message: "Would you like to add more team members?",
-                default: false,
-            },
-        ])
-        .then(employeeData => {
-            let { name, id, email, role, github, school, confirmAddEmployee } = employeeData
-            let employee
+                {
+                    type: "input",
+                    name: "school",
+                    message: "Please enter the intern's school",
+                    when: input => input.role === "Intern",
+                    validate: nameInput => {
+                        if (nameInput) {
+                            return true
+                        } else {
+                            console.log("Please enter the intern's school!")
+                        }
+                    },
+                },
+                {
+                    type: "confirm",
+                    name: "confirmAddEmployee",
+                    message: "Would you like to add more team members?",
+                    default: false,
+                },
+            ])
+            //data for the employee
+            .then(employeeData => {
+                let { name, id, email, role, github, school, confirmAddEmployee } = employeeData
+                let employee
 
-            if (role === "Engineer") {
-                employee = new Engineer(name, id, email, github)
+                if (role === "Engineer") {
+                    employee = new Engineer(name, id, email, github)
 
-                console.log(employee)
-            } else if (role === "Intern") {
-                employee = new Intern(name, id, email, school)
+                    console.log(employee)
+                } else if (role === "Intern") {
+                    employee = new Intern(name, id, email, school)
 
-                console.log(employee)
-            }
+                    console.log(employee)
+                }
 
-            teamArray.push(employee)
+                teamArray.push(employee)
 
-            if (confirmAddEmployee) {
-                return addEmployee(teamArray)
-            } else {
-                return teamArray
-            }
-        })
+                if (confirmAddEmployee) {
+                    return addEmployee(teamArray)
+                } else {
+                    return teamArray
+                }
+            })
+    )
 }
-
+//when the html is created
 const writeFile = data => {
     fs.writeFile("./dist/index.html", data, err => {
         if (err) {
